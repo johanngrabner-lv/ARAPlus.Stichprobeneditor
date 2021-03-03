fe6758217eea6ff36ba1c464f4216a61cbfa9f0a
Johann Grabner <jgrabner@business-tuning.at>
2021-03-03 09:19:01 +0100

    Aufgabenzuteilung

---
+++ ARAPlus.Stichprobeneditor.MVCSample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ARAPlus.Stichprobeneditor.MVCSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARAPlus.Stichprobeneditor.MVCSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(new object());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,30 @@
+Aufgabenpakete: bis 12:30 Uhr
+* Installer, Updates (Friedl)
+* GUI - ASP.NET MVC (Ems-Haselsteiner, Wagner) - Checkliste, Überarbeiterung
+* BL 
+* Datenbank - Offline / Online (Claudia, Markus)
+* Auth, Portal (Felberbauer, Claudia)
+* Doku (Mahr)
+* FileUpload, Web.API, Web-Browser / Tarife-Download (Hoch)
+offen Johann Grabner
+
+Vorschlag: 13:30 Uhr nach der Mittagspause
+Präsentation, NoGos, Lessons Learnend, Schätzung 
+*Github--> DevOps 
+* Präsentation der Ergebenisse
+* Offene Fragen
+* TODOs
+* weiter Action Points
+
+ASP.NET /
+* RAD- Razor Pages <label input-for
+* MVC - Models (VM), Controller - befüllt Model - Repo (DBContext, Repo) --> Action --> Views
+* MVC -- DLL 
+
+
+
+
+ASP.NET MVC
+ASP.Net Web.API (Offline-Client -- neue Tarife, Upload-Server)
+BL (Repo)
+DB
